ca0f4633fecdf624e9beddd873ae759b0e895555
Nagaraj Velusamy <nagaraj.velusamy@nulogic.io>
2024-04-23 22:54:52 +0530

    github build action added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/next-portfolio",
+};
 
 export default nextConfig;
